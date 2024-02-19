d75752a623febb4acfe9d919ae636ec3285b3528
HolbieWan <8841@holbertonstudents.com>
2024-02-19 14:10:43 +0000

    Modified file: 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 	long long int my_long_long = 0;
 	float my_float = 0;
 
-	printf("Size of a char: %ld byte(s)", sizeof(my_char));
-	printf("Size of an int: %ld byte(s)", sizeof(my_int));
-	printf("Size of a long int: %ld byte(s)", sizeof(my_long));
-	printf("Size of a long long int: %ld byte(s)", sizeof(my_long_long));
-	printf("Size of a float: %ld byte(s)", sizeof(my_float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(my_char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(my_int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(my_long));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(my_long_long));
+	printf("Size of a float: %ld byte(s)\n", sizeof(my_float));
 
 	return (0);
 }
