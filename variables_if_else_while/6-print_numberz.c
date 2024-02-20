f57b685a277c3667e2f7b53827af660f59aa43e5
HolbieWan <8841@holbertonstudents.com>
2024-02-20 11:03:44 +0000

    added file: 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* Print alphabet */
+
+/**
+* main - main fonction that prints the text
+* Return: 0 if no problem
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
