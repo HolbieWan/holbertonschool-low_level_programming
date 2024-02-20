d9a3562e79cbd31aded312be58d43ce3167b6c58
HolbieWan <8841@holbertonstudents.com>
2024-02-20 08:35:17 +0000

    Modified file: 1-last_digit.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* Find the last digit */
+
+/**
+* main - main fonction that prints the text
+* Return: 0 if no problem
+*/
+
+int main(void)
+{
+	char alphabet[] = "ABCDEFGHIJKLMNOP\n";
+
+	for (int i = 0; ma_chaine[i] != '\0'; i++)
+	{
+        putchar(ma_chaine[i]);
+    }
+
+	return 0;
+}
