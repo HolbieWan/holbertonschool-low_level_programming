2bdbb4f488517c991a1c4eac313ffc28ccb1a1eb
HolbieWan <8841@holbertonstudents.com>
2024-02-20 10:08:04 +0000

    added file: 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* Print alphabet */
+
+/**
+* main - main fonction that prints the text
+* Return: 0 if no problem
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
