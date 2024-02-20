2a5f2ecc5016359d06761571fcaa016342d774dd
HolbieWan <8841@holbertonstudents.com>
2024-02-20 11:42:36 +0000

    Added file: 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,12 +8,18 @@
 
 int main(void)
 {
-	char i;
+	int i;
+	char j;
 
 	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
 	}
+	for (j = 'a'; j <= 'f'; j++)
+	{
+		putchar(j);
+	}
+
 	putchar('\n');
 
 	return (0);
