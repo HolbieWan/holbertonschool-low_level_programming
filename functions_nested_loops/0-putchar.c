96184e0655757c1c836eed064fc8db8041b40ed5
HolbieWan <8841@holbertonstudents.com>
2024-02-20 14:44:28 +0000

    Added file: 0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* Print alphabet */
+
+/**
+* main - main fonction that prints the text
+* Return: 0 if no problem
+*/
+
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(116);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ variables_if_else_while/10-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* Print alphabet */
+
+/**
+* main - main fonction that prints the text
+* Return: 0 if no problem
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+
+		if (i < 57)
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+
+	return (0);
+}
