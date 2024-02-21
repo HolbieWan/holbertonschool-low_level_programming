4f21b842c077411844c9b074386f3428092a3821
HolbieWan <8841@holbertonstudents.com>
2024-02-21 13:19:27 +0000

    Added files: 5-sign.c ; 5-main.c ; modified main.h

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_isalpha - fonction that prints the sign of a number.
+** @c: The character to print
+* Return: On success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	return (0);
+}
