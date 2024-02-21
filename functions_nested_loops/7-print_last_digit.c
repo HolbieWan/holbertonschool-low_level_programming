a718af5eb3a703772eb91ffa2367d41cc87b0776
HolbieWan <8841@holbertonstudents.com>
2024-02-21 15:36:05 +0000

    Added files: 7-print_last_digit.c ; 7-main.c ; modified main.h

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "6-abs.c"
+
+/**
+*print_last_digit - fonction that prints the last digit of a number.
+** @r: The character to print
+* Return: On success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int print_last_digit(int r)
+{
+	if (r > 0)
+	{
+	r = r % 10;
+	_putchar('0' + r);
+	}
+	if(r == 0)
+	{
+	r = 0;
+	_putchar('0' + r);
+	return(r);
+	}
+	if (r < 0)
+	{
+	r = _abs(r) % 10;
+	_putchar('0' + r);
+	return (r);
+	}
+	return(r);
+}
