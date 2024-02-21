0dd747328a3d56e24ace0eb5a899ad7e0b065105
HolbieWan <8841@holbertonstudents.com>
2024-02-21 14:25:07 +0000

    Added files: 6-abs.c ; 6-main.c ; modified: main.h

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_sign - fonction that prints the sign of a number.
+** @n: The character to print
+* Return: On success 1.
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		r = -1 * r;
+		return (r);
+	}
+	else
+	{
+		return (r);
+	}
+
+	return (0);
+}
