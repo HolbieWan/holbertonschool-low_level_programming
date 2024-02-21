b57c89f83c7aa251d808dafa3d4c25aba055a2df
HolbieWan <8841@holbertonstudents.com>
2024-02-21 12:37:51 +0000

    Modified file: 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,7 +3,9 @@
 
 /**
 *_islower - fonction that checks for lowercase character
-* Return: void
+** @c: The character to print
+* Return: On success 1.
+*On error, -1 is returned, and errno is set appropriately.
 */
 
 int _islower(int c)
