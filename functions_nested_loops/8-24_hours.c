59ee175b8b79ac41a986b9ff741d78707f1c0ea3
HolbieWan <8841@holbertonstudents.com>
2024-02-22 12:18:36 +0000

    Modified files: 8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
 *jack_bauer - fonction that prints every minute of the day of Jack Bauer,
-starting from 00:00 to 23:59.
+*starting from 00:00 to 23:59.
 ** @: The character to print
 * Return: On success 1.
 *On error, -1 is returned, and errno is set appropriately.
