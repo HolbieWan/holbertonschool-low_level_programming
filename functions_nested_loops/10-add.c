4235de3494822e654e7c1044e5fefa66b97902a2
HolbieWan <8841@holbertonstudents.com>
2024-02-22 15:13:28 +0000

    Added files: 10-add.c ; 10-main.c ; Modified main.h

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*add - fonction that adds two integers and returns the result.
+*starting from 00:00 to 23:59.
+** @a: The character to print
+** @b: The character to print
+* Return: On success (a + b).
+*On error, -1 is returned, and errno is set appropriately.
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
