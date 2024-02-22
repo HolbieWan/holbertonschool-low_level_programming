73392c7ecba863dd9f9dd5e266a81eab48d32c65
HolbieWan <8841@holbertonstudents.com>
2024-02-22 21:01:54 +0000

    Modified files: 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,7 +2,6 @@
 
 /**
 *jack_bauer - fonction that prints every minute of the day of Jack Bauer,
-*starting from 00:00 to 23:59.
 ** @: The character to print
 * Return: On success 1.
 *On error, -1 is returned, and errno is set appropriately.
@@ -10,33 +9,31 @@
 
 void times_table(void)
 {
-	int num1;
-	int num2;
-	int result;
-	int digit1;
-	int digit2;
+	int num1, num2, result, digit1, digit2;
 
 	for (num1 = 0; num1 < 10 ; num1++)
 	{
-		for (num2 = 0; num2 < 10 ; num2++)
+		_putchar('0' + 0);
+
+		for (num2 = 1; num2 < 10 ; num2++)
 		{
 			result = (num1 * num2);
 			digit1 = result / 10;
 			digit2 = result % 10;
 
-			if (result < 10 )
+			_putchar(',');
+
+			if (result < 10)
 			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar('0' + result);
-				_putchar(',');
 			}
-			if (result >= 10 )
+			if (result >= 10)
 			{
 				_putchar(' ');
 				_putchar('0' + digit1);
 				_putchar('0' + digit2);
-				_putchar(',');
 			}
 		}
 		_putchar('\n');
