3c0cb1ec10d8502164ac12be0cef641efc642482
HolbieWan <8841@holbertonstudents.com>
2024-02-24 13:15:14 +0000

    Modified file: 101-natural.c

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -6,21 +6,16 @@ int main(void)
 	int a = 3;
 	int b = 5;
 	int i;
-	/** n / 3 = 341,
-	*   n / 5 = 204 */
-	int add3 = 0;
-	int add5 = 0;
 	int total = 0;
 
-	for (i = 0; i <= (n / a) ; i++)
+	for (i = 0; i < n ; i++)
 	{
-		add3 += a * i;
+		if (i % a == 0 || i % b == 0)
+		{
+			total += i;
+		}
 	}
-	for (i = 0; i <= (n / b) - 1 ; i++)
-	{
-		add5 += b * i;
-	}
-	total = add3 + add5;
+
 	printf ("%d\n", total);
 
 	return (0);
