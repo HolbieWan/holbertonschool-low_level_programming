8d65ea3e1b252d3900e450f269c8a9c0dc764718
HolbieWan <8841@holbertonstudents.com>
2024-02-24 12:56:16 +0000

    deleted doubles

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@
 
 void times_table(void)
 {
-	int num1, num2, result, digit1, digit2;
+	int num1, num2, result;
 
 	for (num1 = 0; num1 < 10 ; num1++)
 	{
@@ -18,23 +18,20 @@ void times_table(void)
 		for (num2 = 1; num2 < 10 ; num2++)
 		{
 			result = (num1 * num2);
-			digit1 = result / 10;
-			digit2 = result % 10;
 
 			_putchar(',');
+			_putchar(' ');
 
 			if (result < 10)
 			{
 				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result);
 			}
-			if (result >= 10)
+			else
 			{
-				_putchar(' ');
-				_putchar('0' + digit1);
-				_putchar('0' + digit2);
+				_putchar('0' + result / 10);
 			}
+			_putchar('0' + result / 10);
+			_putchar();
 		}
 		_putchar('\n');
 	}
