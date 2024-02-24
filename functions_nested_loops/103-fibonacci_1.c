ac8a6e2d0b61f9e58c2de0dd6a903cc31db4d5f1
HolbieWan <8841@holbertonstudents.com>
2024-02-24 18:27:32 +0000

    Modified file: 103-fibonacci.c

--- functions_nested_loops/103-fibonacci_1.c
+++ functions_nested_loops/103-fibonacci_1.c
@@ -11,39 +11,37 @@ unsigned long int fibonacci(int n);
 int main(void)
 {
 	unsigned long int limit = 4000000;
-	int i = 1;
-	unsigned long int even_fibonacci_sum = 0;
+	int n = 50;
+	int j;
+	unsigned long int even_fibonacci1_sum = 0;
 	unsigned long int fibonacci_number_i;
 
-	while (1)
+	for (j = 3; j <= n; j++)
 	{
-		fibonacci_number_i = fibonacci(i);
+		fibonacci_number_i = fibonacci(j);
 
 		if (fibonacci_number_i > limit)
 			break;
 
 		if (fibonacci_number_i % 2 == 0)
 		{
-			even_fibonacci_sum += fibonacci_number_i;
+			even_fibonacci1_sum += fibonacci_number_i;
 		}
-
-		i++;
 	}
+	printf("%ld\n", fibonacci_number_i);
 
-	printf("%lu\n", even_fibonacci_sum);
-
-	return 0;
+	return (0);
 }
 
 unsigned long int fibonacci(int n)
 {
-	unsigned long int fibonacci_number1 = 1;
-	unsigned long int fibonacci_number2 = 1;
-	unsigned long int fibonacci_number_i;
 	int i;
+	unsigned long int fibonacci_number1 = 1;
+	unsigned long int fibonacci_number2 = 2;
+	unsigned long int fibonacci_number_i;
 
 	if (n == 1 || n == 2)
-		return 1;
+		return (1);
 
 	for (i = 3; i <= n; i++)
 	{
@@ -51,6 +49,5 @@ unsigned long int fibonacci(int n)
 		fibonacci_number1 = fibonacci_number2;
 		fibonacci_number2 = fibonacci_number_i;
 	}
-
-	return fibonacci_number_i;
+	return (fibonacci_number_i);
 }
