299c821cfb0a79a2027999f1284e72968501610c
HolbieWan <8841@holbertonstudents.com>
2024-02-24 12:52:00 +0000

    Modified file: 100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -8,41 +8,41 @@ void print_times_table(int n)
 {
 	int num1, num2, result;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (num1 = 0; num1 <= n; num1++)
+	for (num1 = 0; num1 <= n; num1++)
     {
-        _putchar('0');
+		_putchar('0');
 
-        for (num2 = 1; num2 <= n; num2++)
-        {
-            result = num1 * num2;
+		for (num2 = 1; num2 <= n; num2++)
+		{
+			result = num1 * num2;
 
-            _putchar(',');
-            _putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 
 			if (result < 10)
-                _putchar(' ');
+			_putchar(' ');
 
 			if (result < 100)
 				_putchar(' ');
 
-            if (result < 10)
-                _putchar('0' + result);
+			if (result < 10)
+			_putchar('0' + result);
 
-            else if (result < 100)
-            {
-                _putchar('0' + result / 10);
-                _putchar('0' + result % 10);
-            }
-            else
-            {
-                _putchar('0' + result / 100);
-                _putchar('0' + result / 10 % 10);
-                _putchar('0' + result % 10);
-            }
-        }
-        _putchar('\n');
-    }
+			else if (result < 100)
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			else
+			{
+				_putchar('0' + result / 100);
+				_putchar('0' + result / 10 % 10);
+				_putchar('0' + result % 10);
+			}
+		}
+		_putchar('\n');
+	}
 }
