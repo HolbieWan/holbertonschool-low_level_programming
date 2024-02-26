990a6f1d18de445264bde7cd765ce3a0380d646f
HolbieWan <8841@holbertonstudents.com>
2024-02-26 09:17:00 +0000

    Modified 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-*_islower - fonction that checks for lowercase character
+*_isupper - fonction that checks for lowercase character
 ** @c: The character to print
 * Return: On success 1.
 *On error, -1 is returned, and errno is set appropriately.
