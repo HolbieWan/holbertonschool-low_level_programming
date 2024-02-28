36170fe2332c790ecf4e468e79c6b7da5e09b0d6
HolbieWan <8841@holbertonstudents.com>
2024-02-28 13:33:18 +0000

    Added files: 9-strcpy.c; 9-main.c; main.h

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ **_strcpy - copys the a string into a buffer
+ *@src: values to copy
+ *@dest: destination of the src values
+ * Returns: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int lenght_src = 0;
+	int i;
+
+	while (src[lenght_src] != '\0')
+	{
+		lenght_src++;
+	}
+
+	for (i = 0; i <= lenght_src ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
