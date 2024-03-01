b76b46330245f028b48b205d62d9293f34e15de2
HolbieWan <8841@holbertonstudents.com>
2024-03-01 12:45:57 +0000

    Added files: 4-rev_array.c; main.c; mofified: main.h

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+
+/**
+ * reverse_array - reverse an array of n elements
+ *@a: inpout string to concatenate
+ *@n: number elements of the array
+ * Return: Always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int lenght_array = 0;
+	int i;
+	int j;
+	char temp;
+
+	while (a[lenght_array] != '\0')
+	{
+		lenght_array++;
+	}
+
+	for (i = 0, j = n - 1; i < j && j <= n; i++, j--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
