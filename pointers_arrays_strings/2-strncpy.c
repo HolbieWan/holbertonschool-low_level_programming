054db0bcd7bdb91ff643d2a1ad9d641010e0306c
HolbieWan <8841@holbertonstudents.com>
2024-03-01 09:14:07 +0000

    Added files: 2-strcpy.c, 2-main.c, main.h

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+
+/**
+ * _strncpy - copy a string to a buffer
+ *@src: inpout string to concatenate
+ *@dest: string to add to dest
+ *@n: number of char to be added
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i <= n && src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
