7ee474f97c029df6d34227a49412247f62024bcf
HolbieWan <8841@holbertonstudents.com>
2024-03-01 11:57:32 +0000

    Modified: 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,18 +3,15 @@
 #include "2-strlen.c"
 
 /**
- * _strncpy - copy a string to a buffer
- *@src: inpout string to concatenate
- *@dest: string to add to dest
- *@n: number of char to be added
+ * _strcmp - copy a string to a buffer
+ *@s1: inpout string to concatenate
+ *@s2: string to add to dest
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int a;
-	int b;
-	int c;
+	int diff;
 	int lenght_s1;
 	int lenght_s2;
 
@@ -26,21 +23,18 @@ int _strcmp(char *s1, char *s2)
 	{
 		if ((s1[i]) == (s2[i]))
 		{
-			a = 0;
-			return (a);
+			diff = 0;
 		}
 		if (s1[i] > s2[i])
 		{
-			b = s1[i] - s2[i];
-			return (b);
+			diff = s1[i] - s2[i];
 			break;
 		}
 		if (s1[i] < s2[i])
 		{
-			c = s1[i] - s2[i];
-			return (c);
+			diff = s1[i] - s2[i];
 			break;
 		}
 	}
-	return (0);
+	return (diff);
 }
