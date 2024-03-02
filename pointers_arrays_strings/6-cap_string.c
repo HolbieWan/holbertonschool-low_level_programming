823707409dc02b24bb5bb6b8d80bea790691ba27
HolbieWan <8841@holbertonstudents.com>
2024-03-02 13:44:49 +0000

    Modified: 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
- *@str: inpout string to concatenate
+ * cap_string - capitalizes all words of a string
+ *@str: inpout string to capitalize
  * Return: Always 0.
  */
 
@@ -19,8 +19,8 @@ char *cap_string(char *str)
 	while (str[i] != ('\0'))
 	{
 		if ((str[i]) == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == ','
-            || str[i] == ';' || str[i] == '!' || str[i] == '?' || str[i] == '"'
-            || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}'
+			|| str[i] == ';' || str[i] == '!' || str[i] == '?' || str[i] == '"'
+			|| str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}'
 			|| str[i] == '.')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
