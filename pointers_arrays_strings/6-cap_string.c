24e696ac5a99b1b84c631e51d17e7243caa7427c
HolbieWan <8841@holbertonstudents.com>
2024-03-02 13:40:33 +0000

    Modified: 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,17 +11,21 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
+	if (str[0] >= 'a' && str[0] <= 'z')
+	{
+		str[0] = str[0] - 32;
+	}
+
 	while (str[i] != ('\0'))
 	{
 		if ((str[i]) == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == ','
             || str[i] == ';' || str[i] == '!' || str[i] == '?' || str[i] == '"'
             || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}'
-			|| str[i] == '.' || str[i] == '\t')
+			|| str[i] == '.')
 		{
-			i++;
-			if (str[i] >= 'a' && str[i] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				str[i] = str[i] - 32;
+				str[i + 1] = str[i + 1] - 32;
 			}
 		}
 		i++;
