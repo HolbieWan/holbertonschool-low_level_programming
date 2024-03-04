f2d25cc5875717593647763748b00da800937ffd
HolbieWan <8841@holbertonstudents.com>
2024-03-04 11:45:34 +0000

    Modified: 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -3,22 +3,21 @@
 
 /**
  * _memcpy() function copies n bytes from memory area src to memory area dest
- * @s: the address of memory to print
- * @b: the replacing string of src to copy in dest
+ * @dest: the address of memory to print
+ * @src: the replacing string of src to copy in dest
  * @n: The number of bytes from memory area src to replace in dest
  * Return: pointer to string dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *dest2 = dest;
-	int i = 0;
+	unsigned int i;
+	unsigned int j;
 
-
-	while (n > 0)
+	for (i = 0, j = 0; j < n; j++, i++)
 	{
-		dest2 [i] = src[n];
-		dest2++;
-		n--;
+		dest2[i] = src[j];
 	}
 	return (dest);
 }
+
