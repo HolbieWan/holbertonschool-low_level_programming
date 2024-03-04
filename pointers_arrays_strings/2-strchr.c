c3b0cc7f92b00ba7ad13574b56106e32cb267e4a
HolbieWan <8841@holbertonstudents.com>
2024-03-04 20:11:01 +0000

    Modified: 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,18 +7,16 @@
  * Return: (s + i): a pointer to the first occurrence of the character
  * returns NULL if char not found
  */
-
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
-		i++;
 	}
 	return (NULL);
 }
