2bbf0b044576e0071eb04bfbbdfbee717955ab92
HolbieWan <8841@holbertonstudents.com>
2024-03-06 22:19:06 +0000

    Modified: 6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,8 +2,8 @@
 #include "5-sqrt_recursion.c"
 
 /**
- * is_prime_number1 - function that returns 1 if the input integer is a prime number,
- * otherwise return 0
+ * is_prime_number1 - function that returns 1 if the input integer
+ * is a prime number, otherwise return 0
  * @n: the number to test
  * @i: the divider
  * Return: 1 if the input integer is a prime number
@@ -22,6 +22,13 @@ int is_prime_number1(int n, int i)
 
 	return (is_prime_number1(n, i + 1));
 }
+
+/**
+ * is_prime_number - function that returns 1 if the input integer
+ * is a prime number, otherwise return 0
+ * @n: the number to test
+ * Return: 1 if the input integer is a prime number
+ */
 
 int is_prime_number(int n)
 {
