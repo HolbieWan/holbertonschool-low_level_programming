6ea2dfb1bd0fcdd5d886277cf47f87f4f5b26280
HolbieWan <8841@holbertonstudents.com>
2024-03-07 14:45:25 +0000

    Added file: 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function of the programme,
+ *prints the number of arguments of function main
+ * @argc: unused argument
+ * @argv: The array of strings
+ * Return: void
+ */
+
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
