4e90d2cb7a01c8e57bf80bce896e87b2ff00134e
HolbieWan <8841@holbertonstudents.com>
2024-03-13 09:46:45 +0000

    Modified: 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,6 +7,7 @@
  ** *string_nconcat - function that concatenates two strings
  * @s1: the 1rst string
  * @s2: the string to be concatenated up to n bytes of s2
+ * @n: the number of bytes of s2 to copy onto s1
  * Return: a pointer to the concatenated string or Null on failure.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -15,29 +16,36 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str_nconcat = NULL;
 
 	if (s1 != NULL)
+	{
 		while (s1[i] != '\0')
 			length_s1 = i++;
+	}
 	if (s2 != NULL)
+	{
 		while (s2[j] != '\0')
 			length_s2 = j++;
-	if (n >= length_s2)
-	{
-		str_nconcat = str_concat(s1, s2);
-		return (str_nconcat);
 	}
+	if (s1 == NULL)
+	{
+		str_nconcat = malloc((n + 1) * sizeof(char));
+		if (str_nconcat == NULL)
+			return (NULL);
+		for (j = 0; j < n; j++)
+			str_nconcat[j] = s2[j];
+	}
+	if (s2 == NULL)
+		return (str_concat(s1, ""));
+	if (n >= length_s2)
+		return (str_concat(s1, s2));
 	if (n < length_s2)
 	{
-		str_nconcat = malloc((length_s1 + n + 2) * sizeof(char));
+		str_nconcat = malloc((length_s1 + n + 1) * sizeof(char));
 		if (str_nconcat == NULL)
 			return (NULL);
 		for (i = 0; i <= length_s1; i++)
-		{
 			str_nconcat[i] = s1[i];
-		}
 		for (j = 0; j < n; j++)
-		{
 			str_nconcat[i + j] = s2[j];
-		}
 		str_nconcat[i + j] = '\0';
 	}
 	return (str_nconcat);
