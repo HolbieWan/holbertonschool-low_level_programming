c55e37ec12300eae6e3b933e4009b1b65444768d
HolbieWan <8841@holbertonstudents.com>
2024-03-13 11:12:23 +0000

    Modified: 1-string_nconcat.c

--- more_malloc_free/1-str_nconcat2.c
+++ more_malloc_free/1-str_nconcat2.c
@@ -15,31 +15,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, length_s1 = 0, length_s2 = 0;
 	char *str_nconcat = NULL;
 
-	if (s1 != NULL)
-		while (s1[i] != '\0')
-			length_s1 = i++;
-	if (s2 != NULL)
-		while (s2[j] != '\0')
-			length_s2 = j++;
-	if (n >= length_s2)
-	{
-		str_nconcat = str_concat(s1, s2);
-		return (str_nconcat);
-	}
-	if (n < length_s2)
-	{
-		str_nconcat = malloc((length_s1 + n + 2) * sizeof(char));
-		if (str_nconcat == NULL)
-			return (NULL);
-		for (i = 0; i <= length_s1; i++)
-		{
-			str_nconcat[i] = s1[i];
-		}
-		for (j = 0; j < n; j++)
-		{
-			str_nconcat[i + j] = s2[j];
-		}
-		str_nconcat[i + j] = '\0';
-	}
+	if (s1 == NULL)
+		str_nconcat = str_concat ("", s2);
+
+	if (s2 == NULL)
+		str_nconcat = str_concat (s1, "");
+
+	while (s1[i] != '\0')
+		length_s1 = i++;
+
+	while (s2[j] != '\0')
+		length_s2 = j++;
+
+	if (n > length_s2)
+		n = length_s2;
+
+	str_nconcat = malloc((length_s1 + n + 1) * sizeof(char));
+	if (str_nconcat == NULL)
+		return (NULL);
+
+	for (i = 0; i <= length_s1; i++)
+		str_nconcat[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		str_nconcat[i + j] = s2[j];
+
+	str_nconcat[i + j] = '\0';
+
 	return (str_nconcat);
 }
