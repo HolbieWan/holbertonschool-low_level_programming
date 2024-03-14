2a7f9bea233b1962d2378a304d2859375e9248ac
HolbieWan <8841@holbertonstudents.com>
2024-03-14 12:33:38 +0000

    Added files: dog.h , 0-main.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef dog_h
+#define dog_h
 /**
  * struct dog - Defines parameters for the dog struct
  * @name: First member
@@ -5,10 +7,12 @@
  * @owner: Third member
  *
  * Description: Defines parameters for the dog struct
- */    
+ */
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-}Dog;
+	char *name;
+	float age;
+	char *owner;
+} Dog;
+
+#endif
