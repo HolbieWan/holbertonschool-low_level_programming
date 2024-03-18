6db4b7ab89dd98ff9900dc5eb24776f1a737f98f
HolbieWan <8841@holbertonstudents.com>
2024-03-18 12:39:47 +0000

    Added files: 1-array_iterator.c , 1-main.c , function_pointers.h

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that executes a function given as
+ * a parameter on each element of an array
+ * @array: array of pointers to functions
+ * @size: size of the array
+ * @action: pointers to the function: action
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
