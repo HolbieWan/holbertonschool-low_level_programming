47d886238ba2fcfb1bc2ac8c2a43e2531de9f41a
HolbieWan <8841@holbertonstudents.com>
2024-03-19 15:32:32 +0000

    Added files: 1-print_numbers.c , 1-main.c ; Modified: 0-sum_them_all.c , variadic_functions.h

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	int x = 0;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < (n - 1))
+		{
+			x = va_arg(args, int);
+			printf("%d%s", x, separator);
+		}
+	}
+	x = va_arg(args, int);
+	printf("%d\n", x);
+
+	va_end(args);
+}
