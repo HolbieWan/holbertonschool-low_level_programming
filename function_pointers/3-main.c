2b52e6c8a08f2ab9192549573b4cd5ab61d3c095
HolbieWan <8841@holbertonstudents.com>
2024-03-20 08:20:39 +0000

    Modified: 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,10 +18,15 @@ int main(int argc, char *argv[])
 
 	s = argv[2];
 
-	if (argc != 4 || argv[2][1] != 0)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
+	}
+	if (argv[2][1] != 0)
+	{
+		printf("Error\n");
+		exit(99);
 	}
 
 	num1 = atoi(argv[1]);
