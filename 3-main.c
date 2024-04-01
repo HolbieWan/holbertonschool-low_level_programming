a4ed4437a0eb8d86d60d494e0926927047669e61
HolbieWan <8841@holbertonstudents.com>
2024-04-01 11:48:34 +0000

    Modified: 3-main.c

--- 3-main.c
+++ 3-main.c
@@ -1,12 +1,43 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+
+int print_number(long int n)
+{
+
+	int char_count = 0;
+
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+		char_count = 1;
+	}
+
+	if (n == 0)
+	{
+		putchar('0');
+		char_count = 1;
+		return (char_count);
+	}
+	if (n / 10)
+	{
+		char_count += print_number(n / 10);
+	}
+	putchar(n % 10 + '0');
+		char_count++;
+
+
+	return (char_count);
+}
+
+
 int main(void)
 {
-    _printf("Test %f %g %c, %d, %s, %%", 'B', 3, "stSchool");
+	int char_count = 0;
+
+    char_count = print_number(12345);
+	printf("\n");
+	printf("%d\n", char_count);
+
     return (0);
 }
