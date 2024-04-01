ed50fe62e47edd27d0a856ffec04597d6d6bf4fc
HolbieWan <8841@holbertonstudents.com>
2024-04-01 16:35:04 +0000

    Added file: 3-add_node_end.c , Updated lists.h with new prototype

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,12 +3,12 @@
 int _strlen(const char *s);
 
 /**
- * add_node - function that adds a new node at the beginning of a list_t list
+ * add_node_end - function that adds a new node at the end of a list_t list
  * @head: pointer to the head node of the list
  * @str: the string member of the list
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 
@@ -19,7 +19,22 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
-	new_node->next = *head;
+	new_node->next = NULL;
+
+	if (list_t **head == NULL)
+		return (new_node);
+
+	else
+	{
+		list_t *temp = list_t *head;
+
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
+	}
+
 	*head = new_node;
 
 	return (new_node);
