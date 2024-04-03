94b9b0b9ac76e7a8cbc5552c8b6d295ffbab27e4
HolbieWan <8841@holbertonstudents.com>
2024-04-03 15:41:35 +0000

    Updated file: 8-delete_dnodeint.c to handle memory leaks

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -25,6 +25,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = temp->next;
 		if (*head != NULL)
 			(*head)->prev = NULL;
+		free(temp);
 		return (1);
 	}
 	if (temp->next != NULL)
