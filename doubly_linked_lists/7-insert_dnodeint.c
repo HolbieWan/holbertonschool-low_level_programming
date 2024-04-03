a2aeb44abacc6d7872927cec748a0e81edd418bf
HolbieWan <8841@holbertonstudents.com>
2024-04-03 11:51:47 +0000

    Added file: 7-insert_dnodeint.c , Updated: lists.h with new prototype

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,43 +11,39 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count = 0;
-
 	dlistint_t *temp;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	if (idx == 0)
-	{
-		new_node->n = n;
-		new_node->next = *h;
-		new_node->prev = NULL;
-
-		if (h != 0)
-		{
-			(*h)->prev = new_node;
-		}
-		*h = new_node;
-
-		return (new_node);
-	}
 
 	temp = *h;
 	new_node->n = n;
 
+	if (new_node == NULL)
+		return (NULL);
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		new_node->prev = NULL;
+		if (h != 0)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+	if (*h == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*h = new_node;
+		return (new_node);
+	}
 	while (count < idx - 1 && temp != NULL)
 	{
 		temp = temp->next;
 		count++;
 	}
-
 	if (temp == NULL)
 		return (NULL);
-
 	new_node->prev = temp;
 	new_node->next = temp->next;
-
 	if (temp->next != NULL)
 	{
 		temp->next->prev = new_node;
@@ -58,6 +54,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp->next = new_node;
 		new_node->prev = temp;
 	}
-	
 	return (new_node);
 }
