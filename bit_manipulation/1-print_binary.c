5092601d28ba97eb411bd1e54d2e5adc96a18bd2
HolbieWan <8841@holbertonstudents.com>
2024-04-05 08:58:15 +0000

    Updated 1-print_binary.c with corrected if else condition and betty style

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * binary_to_uint - function that prints the binary representation of a number.
+ * print_binary - function that prints the binary representation of a number.
  * @n: the number to convert innto binary
  * Return: void
  */
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
-		_putchar(0 + '0');
-	if (n == 1)
-		_putchar(1 + '0');
+		_putchar(0);
+	else if (n == 1)
+		_putchar(1);
 
-	if (n > 0)
+	else if (n > 0)
 	{
 		print_binary(n >> 1);
-		_putchar ((n & 1) + '0');
 	}
+	_putchar ((n & 1) + '0');
 }
