a4c1fb0b84fc5666645798341a88db610d50ae8a
HolbieWan <8841@holbertonstudents.com>
2024-04-07 15:03:54 +0000

    Updated: 2-append_text_to_file.c with corrected betty style and handle case when test_content == NULL

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - function that appends text at the end of a file.
+ * append_text_to_file - function that appends text at the end of a file.
  * and prints it to the POSIX standard output.
  * @filename:name of the file to be appended
  * @text_content: a NULL terminated string to append to the file
