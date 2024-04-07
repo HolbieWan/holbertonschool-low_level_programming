230b0d60e4a1354f9489ceaabde58710170c983c
HolbieWan <8841@holbertonstudents.com>
2024-04-07 15:08:29 +0000

    Updated: 2-append_text_to_file.c to handle case when test_content == NULL

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -25,7 +25,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		nbletters++;
 
 	if (text_content == NULL)
-		return (1);
+	{
+		;
+	}
 
 	if (text_content != NULL)
 		nbwritten = write(fd, text_content, nbletters);
