bb22c7e69f2d22a2eeec2ab9030b1e09b3b05e6f
HolbieWan <8841@holbertonstudents.com>
2024-04-07 15:03:36 +0000

    Updated: 2-append_text_to_file.c with corrected betty style and handle case when test_content == NULL

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -24,6 +24,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	while (text_content[nbletters] != '\0')
 		nbletters++;
 
+	if (text_content == NULL)
+		return (1);
+
 	if (text_content != NULL)
 		nbwritten = write(fd, text_content, nbletters);
 
