3f4645af1e2d371d9ed5465971b778759b736a09
HolbieWan <8841@holbertonstudents.com>
2024-08-30 13:32:12 +0200

    Cleaning repo

--- Mock_interview_3/16-print_diagonal.py
+++
@@ -1,8 +0,0 @@
-#!usr/bin/python3
-def print_diagonal(n):
-    for i in range(1, n + 1):
-        print("\\")
-        for j in range(0, i):
-            print(" ", end="")
-
-print_diagonal(5)
--- Mock_interview_3/List _power_2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-my_list=[1, 2, 3, 4, 5]
-new_list = list(map(lambda x: x ** 2, my_list))
-
-print(new_list)
-print(my_list)
